6d604a9263018f027a4b8d1c56c7d6221db5efd7
Alex Akbar <alexampp11@gmail.com>
2021-06-04 22:38:47 +0700

    Fix initial name on write comment

--- lib/components/items/item_post.dart
+++ lib/components/items/item_post.dart
@@ -241,9 +241,7 @@ class _ItemPostState extends State<ItemPost> with TickerProviderStateMixin {
                         ),
                         child: Center(
                           child: Text(
-                            widget.post.user.fullname
-                                .substring(0, 1)
-                                .toUpperCase(),
+                            user.fullname.substring(0, 1).toUpperCase(),
                             style: Themes().whiteBold14,
                           ),
                         ),
